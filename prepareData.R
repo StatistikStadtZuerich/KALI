a87471d35c519d014f2d9c453286048ae50babca
Metzler Aline (SSZ) <Aline.Metzler@zuerich.ch>
2022-08-31 09:49:09 +0200

    update data load

--- prepareData.R
+++ prepareData.R
@@ -16,16 +16,49 @@ package.check <- lapply(
   }
 )
 
-### Load Data
+## Join data
 data_2022 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik_gemeinderatswahlen_2022_kandidierende/download/gemeinderatswahlen_2022_kandidierende.csv", encoding = "UTF-8") %>% mutate(Wahljahr = 2022) %>% rename(Nachname = X.U.FEFF.Nachname)
 data_2018 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik-gemeinderatswahlen-2018-alle-kandidierenden/download/GRW-2018-alle-Kandidierenden-OGD.csv", encoding = "UTF-8") %>% mutate(Wahljahr = 2018)
 data_2014 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik-gemeinderatswahlen-2014-alle-kandidierenden/download/GRW-2014-alle-Kandidierenden-OGD.csv", encoding = "UTF-8") %>% mutate(Wahljahr = 2014)
 data_2010 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik-gemeinderatswahlen-2010-alle-kandidierenden/download/GRW-2010-alle-Kandidierenden-OGD.csv", encoding = "UTF-8") %>% mutate(Wahljahr = 2010)
 data_2006 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik-gemeinderatswahlen-2006-alle-kandidierenden/download/GRW-2006-alle-Kandidierenden-OGD.csv", encoding = "UTF-8") %>% mutate(Wahljahr = 2006)
 
-## Join data
 df <- data_2022 %>% 
   bind_rows(data_2018, data_2014, data_2010, data_2006)
 
 rm(data_2022, data_2018, data_2014, data_2010, data_2006)
+
+
+### Load Data
+data22 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik_gemeinderatswahlen_2022_resultate/download/GRW_2022_resultate_kandidierende_und_herkunft_der_stimmen.csv", encoding = "UTF-8") %>% mutate(Wahljahr = 2022)
+data18 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik_gemeinderatswahlen_2018_resultate/download/GRW_2018_Resultate_und_herkunft_der_stimmen.csv", encoding = "UTF-8") %>% mutate(Wahljahr = 2018)
+data14 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik-gemeinderatswahlen-2014-resultate/download/GRW_2014_Resultate_und_Herkunft_der_Stimmen_Nachzahlung_v2.csv", encoding = "UTF-8") %>% mutate(Wahljahr = 2014)
+data10 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik-gemeinderatswahlen-2010-resultate/download/GRW_2010_Resultate_und_Herkunft_der_Stimmen.csv", sep = ";", encoding = "UTF-8") %>% mutate(Wahljahr = 2010) %>% rename(Liste_Bez_lang = Liste, Wahlresultat = Wahlergebnis)
+# data06 <- read.csv("https://data.stadt-zuerich.ch/dataset/politik-gemeinderatswahlen-2006-alle-kandidierenden/download/GRW-2006-alle-Kandidierenden-OGD.csv", encoding = "UTF-8") %>% mutate(Wahljahr = 2006)
+
+
+## Function to make data long to wide
+data_prep <- function(data){
+  data %>% 
+    select(Wahljahr, Liste_Bez_lang, Wahlkreis, Nachname, Vorname, Wahlresultat, total_stim, starts_with("part"), starts_with("stim")) %>% 
+    gather(Var, Value, -Wahljahr, -Liste_Bez_lang, -Wahlkreis, -Nachname, -Vorname, -Wahlresultat, -total_stim, -starts_with("part")) %>% 
+    mutate(Value = as.numeric(Value))
+    
+}
+
+
+df22 <- data_prep(data22)
+df18 <- data_prep(data18)
+df14 <- data_prep(data14)
+df10 <- data_prep(data10)
+
+df_det <- df22 %>% 
+  bind_rows(df18, df14, df10)
+
+rm(data22, data18, data14, data10, df22, df18, df14, df10)
+
+
+
+
+
 
